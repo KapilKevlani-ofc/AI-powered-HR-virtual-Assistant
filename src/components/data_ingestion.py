bc0fe91c3df3ef6ff417011c89f542a66210372b
KapilKevlani-ofc <kapilkevlani@promactinfo.com>
2024-06-18 10:57:30 +0530

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,13 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.document_loaders import DirectoryLoader
+import csv
 def load_pdf():
    loader = DirectoryLoader('./data', glob="./*.pdf", loader_cls=PyMuPDFLoader)
    documents = loader.load()
    return documents
+
+def store_timings_to_csv(process_name, elapsed_time):
+       csv_file_path = "./latency_track.csv"
+       with open(csv_file_path, mode='a', newline='') as file:
+           writer = csv.writer(file)
+           writer.writerow([process_name, elapsed_time])
